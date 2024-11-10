107f5a20a792fff9e40114652bc68600f4da9b28
pasho12 <152061735+pasho12@users.noreply.github.com>
2024-11-10 13:32:18 +0500

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,7 +7,8 @@ public class Scripting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    //хз чтьо писавть
+       print("Hello, World!";); 
     }
 
     // Update is called once per frame
